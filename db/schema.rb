8dfd43f16295c7f0a79adbb4807beb0f81229127
Ryan Timmons <timmonsryan@ymail.com>
2014-10-12 13:52:08 -0500

    Adds giveaway? boolean to events model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012182902) do
+ActiveRecord::Schema.define(version: 20141012185116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141012182902) do
     t.string   "picture"
     t.float    "guestCost"
     t.boolean  "locked"
+    t.boolean  "giveaway"
   end
 
   create_table "users", force: true do |t|
