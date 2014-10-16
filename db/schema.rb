6b186f7b952ab60b8dd6bc3a2da30b028b56e325
Ryan Timmons <timmonsryan@ymail.com>
2014-10-15 23:16:52 -0500

    Adds Post model and related specs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012185116) do
+ActiveRecord::Schema.define(version: 20141016040210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 20141012185116) do
     t.boolean  "locked"
     t.boolean  "giveaway"
   end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "tellerNumber"
