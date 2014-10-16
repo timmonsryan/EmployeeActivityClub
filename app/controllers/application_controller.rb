95c35b2a50a89983f90fb2a3a8e9f7ce31a6c75e
Ryan Timmons <timmonsryan@ymail.com>
2014-10-15 23:14:44 -0500

    Adds verifyAdmin controller method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+  	def verifyAdmin
+  		unless currentUser.admin?
+  			flash[:error] = "You must be an admin to access that."
+  			redirect_to root_path
+  		end
+  	end
 end
